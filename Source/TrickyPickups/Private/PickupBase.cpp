56774bd5979e570d9f672c71d297c59fe2a2d699
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-31 21:11:31 +0200

    Create a base pickup class

---
+++ Source/TrickyPickups/Private/PickupBase.cpp
@@ -0,0 +1,72 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "PickupBase.h"
+
+
+APickupBase::APickupBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void APickupBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APickupBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APickupBase::ActivatePickup(AActor* TargetActor)
+{
+	if (!IsValid(TargetActor))
+	{
+		return;
+	}
+
+	if (ActivatePickupEffect(TargetActor))
+	{
+		OnPickupEffectActivated(TargetActor);
+		OnPickupActivated.Broadcast();
+
+		if (bDestroyOnActivation)
+		{
+			Destroy();
+		}
+		else
+		{
+			DisablePickup();
+		}
+	}
+}
+
+void APickupBase::EnablePickup()
+{
+	if (!IsHidden())
+	{
+		return;
+	}
+
+	SetActorHiddenInGame(false);
+	SetActorEnableCollision(true);
+}
+
+bool APickupBase::ActivatePickupEffect_Implementation(AActor* TargetActor)
+{
+	return true;
+}
+
+void APickupBase::DisablePickup()
+{
+	if (IsHidden())
+	{
+		return;
+	}
+	
+	SetActorHiddenInGame(true);
+	SetActorEnableCollision(false);
+	OnPickupDisabled();
+}
+
---
+++ Source/TrickyPickups/Public/PickupBase.h
@@ -0,0 +1,55 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickupBase.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPickupActivatedSignature);
+
+UCLASS()
+class TRICKYPICKUPS_API APickupBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APickupBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintAssignable, Category="Pickup")
+	FOnPickupActivatedSignature OnPickupActivated;
+
+	UFUNCTION
+	(BlueprintCallable, Category="Pickup")
+	void ActivatePickup(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable, Category="Pickup")
+	virtual void EnablePickup();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Pickup")
+	bool bDestroyOnActivation = true;
+	
+	UFUNCTION(BlueprintNativeEvent, Category="Pickup")
+	bool ActivatePickupEffect(AActor* TargetActor);
+
+	virtual bool ActivatePickupEffect_Implementation(AActor* TargetActor);
+
+private:
+	UFUNCTION(BlueprintImplementableEvent, Category="Pickup")
+	bool OnPickupEffectActivated(AActor* TargetActor);
+
+	virtual void DisablePickup();
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Pickup")
+	void OnPickupEnabled();
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Pickup")
+	void OnPickupDisabled();
+};
