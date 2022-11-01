a73bd170115a176605e776a34555e4786a1390c3
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-11-01 23:15:21 +0200

    Describe the PickupBase class

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # TrickyPickups
+
 A plugin with different base pickup classes
+
+## Dependencies
+
+This plugin required this plugins to work:
+
+1. [**TrickyInteractionSystem**](https://github.com/TrickyFatCat/TrickyInteractionSystem);
+2. [**TrickyAnimationComponents**](https://github.com/TrickyFatCat/TrickyAnimationComponents);
+
+Install them before installing this plugin.
+
+## Installation
+
+The plugin can be used in both C++ and Blueprint projects.
+
+### Blueprint projects
+
+**At the moment, the package is compatible only with Unreal Engine 4.**
+
+1. Download package file;
+2. Unzip the package to the Plugins folder in engine folder, e.g. `C:\Program Files\Epic Games\UE_4.27\Engine\Plugins`;
+3. Restart the project;
+
+In this case the plugin can be used for any blueprint project.
+
+### C++ projects
+
+1. Create the Plugins folder in the project directory;
+2. Create the TrickyAnimationComponents folder in the Plugins folder;
+3. Download the plugin source code into that folder;
+4. Rebuild the project;
+
+## Content
+
+The plugin contains actors:
+
+1. PickupBase;
+2. PickupNormal;
+3. PickupInteractive;
+
+### PickupBase
+
+A base pickup class which contains the main logic.
+
+#### Variables
+
+1. `DestroyOnActivation` - if true the pickup actor will destroy on activation, else it'll be disabled and hidden in game;
+2. `InterpolateToTarget` - if true the actor will be interpolated to the target using EaseAnimationComponent from [**
+   TrickyAnimationComponents**](https://github.com/TrickyFatCat/TrickyAnimationComponents);
+3. `ActivationDistance` - determines the distance from the target actor the `ActivatePickup` function will be called.
+   Worked only if `InterpolateToTarget == true`;
+
+#### Functions
+
+1. `ActivatePickup` - activates the pickup logic. Call this function if you want to activate the pickup in your custom
+   pickup class;
+2. `EnablePickup` - enables pickup if it was disabled when `DestroyOnActivation == false`;
+3. `PickupEffect` - a function which is called to activate the actual pickup effect. Override it to implement pickup
+   effect;
+
+#### Events
+
+1. `OnPickupEffectActivated` - called when the pickup effect was successfully activated;
+2. `OnPickupEnabled` - called when the pickup was enabled;
+3. `OnPickupDisabled` - called when the pickup was disabled;
+
+#### Delegates
+
+1. `OnPickupActivated` - called when the pickup effect was successfully activated;
+
+### PickupNormal
+
+### PickupInteractive
--- Source/TrickyPickups/Public/PickupBase.h
+++ Source/TrickyPickups/Public/PickupBase.h
@@ -10,6 +10,9 @@ class UEaseAnimationComponent;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPickupActivatedSignature);
 
+/**
+ * A base pickup class which contains the main logic.
+ */
 UCLASS()
 class TRICKYPICKUPS_API APickupBase : public AActor
 {
@@ -24,13 +27,23 @@ protected:
 public:
 	virtual void Tick(float DeltaTime) override;
 
+	/**
+	 * Called when the pickup effect was successfully activated;
+	 */
 	UPROPERTY(BlueprintAssignable, Category="Pickup")
 	FOnPickupActivatedSignature OnPickupActivated;
 
-	UFUNCTION
-	(BlueprintCallable, Category="Pickup")
+	/**
+	 * Activates the pickup logic.
+	 * Call this function if you want to activate the pickup in your custom pickup class.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Pickup")
 	bool ActivatePickup(AActor* OtherActor);
 
+	/**
+	 * Enables the pickup if it was disabled.
+	 * Works if DestroyOnActivation == false.
+	 */
 	UFUNCTION(BlueprintCallable, Category="Pickup")
 	virtual void EnablePickup();
 
@@ -38,34 +51,58 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess))
 	UEaseAnimationComponent* EaseAnimationComponent = nullptr;
 
+	/**
+	 * If true the pickup actor will destroy on activation, else it'll be disabled and hidden in game.
+	 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Pickup")
 	bool bDestroyOnActivation = true;
 
+	/**
+	 * If true the actor will be interpolated to the target using EaseAnimationComponent.
+	 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Pickup")
 	bool bInterpolateToTarget = false;
-	
+
+	/**
+	 * A function which is called to activate the actual pickup effect. Override it to implement pickup effect.
+	 */
 	UFUNCTION(BlueprintNativeEvent, Category="Pickup")
 	bool PickupEffect(AActor* OtherActor);
 
 	virtual bool PickupEffect_Implementation(AActor* OtherActor);
 
+	/**
+	 * Called when the pickup effect was successfully activated.
+	 */
 	UFUNCTION(BlueprintImplementableEvent, Category="Pickup")
 	void OnPickupEffectActivated(AActor* OtherActor);
 
 	virtual void DisablePickup();
 
+	/**
+	 * Called when the pickup was enabled.
+	 */
 	UFUNCTION(BlueprintImplementableEvent, Category="Pickup")
 	void OnPickupEnabled();
 
+	/**
+	 * Called when the pickup wan disabled.
+	 */
 	UFUNCTION(BlueprintImplementableEvent, Category="Pickup")
 	void OnPickupDisabled();
 
 private:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Pickup", meta=(AllowPrivateAccess, EditCondition="bInterpolateToTarget"))
+	/**
+	 * Determines the distance from the target actor the ActivatePickup function will be called.
+	 */
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadWrite,
+		Category="Pickup",
+		meta=(AllowPrivateAccess, EditCondition="bInterpolateToTarget"))
 	float ActivationDistance = 32.f;
-	
+
 	UPROPERTY()
 	AActor* TargetActor = nullptr;
-	
+
 	bool ActivatePickupEffect();
 };
